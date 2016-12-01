e991c767dcd8381ae791d8aa3c37fcdcf7cdff2f
Katekarin <gmmetin7@gmail.com>
2016-12-02 00:08:15 +0100

    yml

--- travis/sqf_validator.py
+++ travis/sqf_validator.py
@@ -157,9 +157,9 @@ def main():
     args = parser.parse_args()
 
     # Allow running from root directory as well as from inside the tools directory
-    rootDir = "../addons"
-    if (os.path.exists("addons")):
-        rootDir = "addons"
+    rootDir = "../StanLakeside.Jackson_County"
+    if (os.path.exists("StanLakeside.Jackson_County")):
+        rootDir = "StanLakeside.Jackson_County"
 
     for root, dirnames, filenames in os.walk(rootDir + '/' + args.module):
       for filename in fnmatch.filter(filenames, '*.sqf'):
